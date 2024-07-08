91702b946748d0ec3325031ffc3aa3a720845a74
abhikrokr <abhikrokre6@gmail.com>
2024-07-08 19:02:58 +0530

    Add files via upload

---
+++ challenge/src/main/java/com/dws/challenge/web/AccountsController.java
@@ -0,0 +1,72 @@
+package com.dws.challenge.web;
+
+import com.dws.challenge.domain.Account;
+import com.dws.challenge.domain.MoneyTransfer;
+import com.dws.challenge.domain.MoneyTransferResponse;
+import com.dws.challenge.exception.AccountIdNotFoundException;
+import com.dws.challenge.exception.DuplicateAccountIdException;
+import com.dws.challenge.exception.MinTransferAmountException;
+import com.dws.challenge.exception.NotEnoughBalanceException;
+import com.dws.challenge.service.AccountsService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/v1/accounts")
+@Slf4j
+public class AccountsController {
+
+  private final AccountsService accountsService;
+
+  @Autowired
+  public AccountsController(AccountsService accountsService) {
+    this.accountsService = accountsService;
+  }
+
+  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Object> createAccount(@RequestBody @Valid Account account) {
+    log.info("Creating account {}", account);
+
+    try {
+    this.accountsService.createAccount(account);
+    } catch (DuplicateAccountIdException daie) {
+      return new ResponseEntity<>(daie.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    return new ResponseEntity<>(HttpStatus.CREATED);
+  }
+
+  @GetMapping(path = "/{accountId}")
+  public Account getAccount(@PathVariable String accountId) {
+    log.info("Retrieving account for id {}", accountId);
+    return this.accountsService.getAccount(accountId);
+  }
+
+  @PostMapping("/transferMoney")
+  public ResponseEntity<Object> transferMoney(@RequestBody @Valid MoneyTransfer moneyTransfer) {
+    log.info("Money transfer request from  account : {} to account:{} of amount: {}"
+            , moneyTransfer.getAccountFrom(),moneyTransfer.getAccountTo(),moneyTransfer.getAmount());
+
+    try {
+      this.accountsService.transferMoney(moneyTransfer);
+    } catch (AccountIdNotFoundException e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    } catch (NotEnoughBalanceException | MinTransferAmountException e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+}
