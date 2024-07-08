82a7bf0323963510cd53ed13c6e7de39fb3b0434
abhikrokr <abhikrokre6@gmail.com>
2024-07-08 16:37:27 +0530

    Delete challenge directory

--- challenge/src/main/java/com/dws/challenge/exception/NotEnoughBalanceException.java
+++
@@ -1,8 +0,0 @@
-package com.dws.challenge.exception;
-
-public class NotEnoughBalanceException extends RuntimeException {
-
-  public NotEnoughBalanceException() {
-    super("Not Enough Balance");
-  }
-}
