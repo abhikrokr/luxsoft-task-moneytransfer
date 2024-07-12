ea63a40e75b39064b5e1ec02e45e37727a77f1dd
abhikrokr <abhikrokre6@gmail.com>
2024-07-12 12:51:05 +0530

    Delete src directory

--- src/main/java/com/dws/challenge/exception/MinTransferAmountException.java
+++
@@ -1,8 +0,0 @@
-package com.dws.challenge.exception;
-
-public class MinTransferAmountException extends RuntimeException {
-
-  public MinTransferAmountException() {
-    super("Minimum transfer amount should be more than 0.1");
-  }
-}
