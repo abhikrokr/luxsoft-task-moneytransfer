429f7f47666129f6a383e3ee970f2f7e28177fa8
abhikrokr <abhikrokre6@gmail.com>
2024-07-12 12:50:15 +0530

    Delete Another approach directory

--- Another approach/src/main/java/com/dws/challenge/exception/AccountIdNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.dws.challenge.exception;
-
-public class AccountIdNotFoundException extends RuntimeException {
-
-  public AccountIdNotFoundException(String message) {
-    super("Account not found : "+message);
-  }
-}
